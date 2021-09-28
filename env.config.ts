59bbfe4a8564e426e83a4d3b2a19a3b393b109c2
Sean Luo <shawnluo415@gmail.com>
2021-09-27 22:22:03 -0700

    d

--- env.config.ts
+++ env.config.ts
@@ -10,8 +10,8 @@ if (process.env.NODE_ENV === "production") {
   client_port = "http://rmtlab-env.eba-vprm97ju.us-east-2.elasticbeanstalk.com";
   multerUpload = multerUploadAWS;
 } else {
-  // client_port = "http://localhost:3000";
-  client_port = "http://localhost:8000";
+  client_port = "http://localhost:3000";
+  // client_port = "http://localhost:8000";
   multerUpload = multerUploadLocal;
 }
 
--- prisma/migrations/20210928013349_user_table/migration.sql
+++ prisma/migrations/20210928013349_user_table/migration.sql
@@ -3,6 +3,10 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "profileImg" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
